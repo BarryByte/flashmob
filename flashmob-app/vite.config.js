b4d609407f46242f3e655d42d799f574370d2e88
BarryByte <abhayraj.12667@gmail.com>
2025-04-02 01:22:17 +0530

    proxy

--- flashmob-app/vite.config.js
+++ flashmob-app/vite.config.js
@@ -7,11 +7,6 @@ export default defineConfig({
   plugins: [react(),tailwindcss(),],
   server: {
     proxy: {
-      "/api/huggingface": {
-        target: "https://router.huggingface.co",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/huggingface/, ""),
-      },
       '/generate_questions': 'http://127.0.0.1:5000',
     },
   }
