8469d531a4f71bc7ece3f5e4dcb684376c51174b
BarryByte <abhayraj.12667@gmail.com>
2025-04-02 00:20:26 +0530

    env update

--- flashmob-app/src/firebase.jsx
+++ flashmob-app/src/firebase.jsx
@@ -4,16 +4,19 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // Import Authentication
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
+
+console.log("Firebase Config:", firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
